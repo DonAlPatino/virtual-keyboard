e37e01659f9a174b561bd3dd26c186de767bea04
DonAlPatino <valp@inbox.ru>
2023-04-25 17:49:35 +0300

    fix: resolve eslint errors for this in class and decostruction for array

--- keyboard.js
+++ keyboard.js
@@ -2,19 +2,19 @@ import keysEnglish from './keys.js';
 
 class Keyboard {
   constructor(body) {
-    const keys = keysEnglish;
+    this.keys = keysEnglish;
     const keyboard = document.createElement('div');
     keyboard.classList.add('keyboard');
-    const buttonCodes = Object.keys(keys);
-    keyboard.appendChild(this.createRowButtons(keys, buttonCodes.slice(0, 14)));
-    keyboard.appendChild(this.createRowButtons(keys, buttonCodes.slice(14, 29)));
-    keyboard.appendChild(this.createRowButtons(keys, buttonCodes.slice(29, 42)));
-    keyboard.appendChild(this.createRowButtons(keys, buttonCodes.slice(42, 55)));
-    keyboard.appendChild(this.createRowButtons(keys, buttonCodes.slice(55, buttonCodes.length)));
+    const buttonCodes = Object.keys(this.keys);
+    keyboard.appendChild(this.createRowButtons(buttonCodes.slice(0, 14)));
+    keyboard.appendChild(this.createRowButtons(buttonCodes.slice(14, 29)));
+    keyboard.appendChild(this.createRowButtons(buttonCodes.slice(29, 42)));
+    keyboard.appendChild(this.createRowButtons(buttonCodes.slice(42, 55)));
+    keyboard.appendChild(this.createRowButtons(buttonCodes.slice(55, buttonCodes.length)));
     body.appendChild(keyboard);
   }
 
-  createRowButtons = (keys,arr) => {
+  createRowButtons(arr) {
     const rowKeys = document.createElement('div');
     rowKeys.classList.add('keyboard__row');
     arr.forEach((item) => {
@@ -22,29 +22,26 @@ class Keyboard {
       button.classList.add('keyboard__key');
       button.ariaLabel = item;
 
-      if (Array.isArray(keys[item])) {
-        if (keys[item][0].match(/[a-zа-яё]/)) {
-          button.textContent = keys[item][0];
-        }
-        else
-        {
+      if (Array.isArray(this.keys[item])) {
+        if (this.keys[item][0].match(/[a-zа-яё]/)) {
+          // eslint-disable-next-line prefer-destructuring
+          button.textContent = this.keys[item][0];
+        } else {
           const up = document.createElement('div');
-          up.textContent = keys[item][1];
-          button.appendChild(up);
           const down = document.createElement('div');
-          down.textContent = keys[item][0];
+          [down.textContent, up.textContent] = this.keys[item];
+          button.appendChild(up);
           button.appendChild(down);
           button.classList.add('keyboard__key_spec');
         }
-      }
-      else {
-        if (keys[item].match(/Backspace|Del|Tab|CapsLock|Enter|Shift| /)) {
+      } else {
+        if (this.keys[item].match(/Backspace|Del|Tab|CapsLock|Enter|Shift| /)) {
           button.classList.add('keyboard__key_wide');
         }
-        if (keys[item].match(/Ctrl/)) {
+        if (this.keys[item].match(/Ctrl/)) {
           button.classList.add('keyboard__key_double');
         }
-          button.textContent = keys[item];
+        button.textContent = this.keys[item];
       }
 
       button.dataset.code = item;
